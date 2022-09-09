a7227c207999d7e960d426b12e7c41a39d068a3d
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2022-09-09 17:03:36 +0530

    Create Counting smaller.cpp
    Mocha is given yet another task. In the task mocha is provided with a sequence of n numbers. In the sequence for every number we have to find the count of elements that are strictly smaller than it and generate a sequence from it. Your task is to help mocha in building the sequence.
    
    Input Format
    The first line contains an integer n
    
    The second line contains n integers nums1,nums2…..numsn.
    
    Output Format
    The output contains n integers where every integer represents the count of smaller elements.
    
    Constraints
    1<=n<=100000
    
    1<=nums[n]<=100
    
    Sample Testcase 0
    Testcase Input
    5
    8 1 2 2 3
    Testcase Output
    4 0 1 1 3 
    Explanation
    For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
    For nums[1]=1 does not exist any smaller number than it.
    For nums[2]=2 there exist one smaller number than it (1). 
    For nums[3]=2 there exist one smaller number than it (1). 
    For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
    Sample Testcase 1
    Testcase Input
    4
    7 7 7 7 
    Testcase Output
    0 0 0 0
    Explanation
    As all elements are same thus answer vector consists of all 0s.
    Unstop
    C

---
+++ Counting smaller.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1e9 + 7
+typedef long long ll;
+
+
+// function for execution
+vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        // finding size of nums
+        int n = nums.size();
+        // creating bucket of size 102
+        int bucket[102] = {0};
+        // looping through nums
+        for(auto i:nums){
+            bucket[i+1]++;
+        }
+        // filling up bucket vector
+        for(int i=1;i<102;i++){
+            bucket[i] += bucket[i-1];
+        }
+        // filling up nums
+        for(int i=0;i<n;i++){
+            nums[i] = bucket[nums[i]];
+        }
+
+
+        // returning
+        return nums;
+}
+
+
+// driver code for execution
+int main(){
+    
+    int n;
+    cin>>n;
+
+   
+    vector<int>arr(n);
+
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+
+    vector<int>ans=smallerNumbersThanCurrent(arr);
+
+
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+
+
+     return 0;
+}
